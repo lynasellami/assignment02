abd90e142002cc8743dacc3079cc29cbee07ce61
lynasellami <2488446@edu.vaniercollege.qc.ca>
2025-11-22 22:56:28 -0500

    Initial commit

---
+++ src/Controller/MarathonController.java
@@ -0,0 +1,28 @@
+package Controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class MarathonController {
+
+    @FXML
+    private Label runnerLabel;
+
+    @FXML
+    private Label distanceLabel;
+
+    @FXML
+    private Button playPauseButton;
+
+    @FXML
+    private Button resetButton;
+
+    @FXML
+    public void initialize() {
+        // TODO: Initialize model and setup UI components
+    }
+
+    // TODO: Add methods to handle button actions
+}
+
---
+++ src/Model/Runner.java
@@ -0,0 +1,12 @@
+package Model;
+
+public class Runner {
+    // TODO: Define attributes (e.g., name, speed, distance)
+    // TODO: Add constructors, getters, and setters
+
+    public Runner() {
+        // TODO: Initialize runner properties
+    }
+
+    // TODO: Add methods (e.g., update distance, reset, etc.)
+}
